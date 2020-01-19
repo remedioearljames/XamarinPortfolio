57fd5709170b24f6a4e340590b1a5f34e7571380
remedioearljames <remedioearljames@gmail.com>
2020-01-19 15:26:14 +1300

    Init

---
+++ XamarinPortfolio/XamarinPortfolio.Android/MainActivity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinPortfolio.Droid
+{
+    [Activity(Label = "XamarinPortfolio", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(UXDivers.Gorilla.Droid.Player.CreateApplication(
+              this,
+              new UXDivers.Gorilla.Config("Good Gorilla")
+                // Register Grial Shared assembly
+                .RegisterAssemblyFromType<UXDivers.Artina.Shared.CircleImage>()
+                // Register UXDivers Effects assembly
+                .RegisterAssembly(typeof(UXDivers.Effects.Effects).Assembly)
+                // FFImageLoading.Transformations
+                .RegisterAssemblyFromType<FFImageLoading.Transformations.BlurredTransformation>()
+                // FFImageLoading.Forms
+                .RegisterAssemblyFromType<FFImageLoading.Forms.CachedImage>()
+              ));
+        }
+    }
+}
---
+++ XamarinPortfolio/XamarinPortfolio.iOS/AppDelegate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace XamarinPortfolio.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(UXDivers.Gorilla.iOS.Player.CreateApplication(
+              new UXDivers.Gorilla.Config("Good Gorilla")
+                  // Register Grial Shared assembly
+                  .RegisterAssemblyFromType<UXDivers.Artina.Shared.CircleImage>()
+                  // Register UXDivers Effects assembly
+                  .RegisterAssembly(typeof(UXDivers.Effects.Effects).Assembly)
+                  // FFImageLoading.Transformations
+                  .RegisterAssemblyFromType<FFImageLoading.Transformations.BlurredTransformation>()
+                  // FFImageLoading.Forms
+                  .RegisterAssemblyFromType<FFImageLoading.Forms.CachedImage>()
+                ));
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
